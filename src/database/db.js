0988d14f10f4bca5515c7464e39fb40274776dd4
Thiago <thiagosousaa84@gmail.com>
2025-04-02 16:03:55 -0300

    second commit

--- src/database/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose"
-
-async function connectDataBase(){
-    await mongoose.connect(
-        "mongodb+srv://thiagoocarvalho:LoRVlNc7wNBBXUzv@cluster0.bhouxas.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    )
-}
-
-export default connectDataBase
